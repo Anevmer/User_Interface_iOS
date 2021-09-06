940cc7624fc0009b22265a636299e222849c7557
Anatoly <annevmerzhitsky@yandex.ru>
2021-09-06 22:46:38 +0300

    Added sorting for friends, added search bar for search communities

--- VkontakteNevmerApp/VkontakteNevmerApp/Base/Extensions/UIColor.swift
+++ VkontakteNevmerApp/VkontakteNevmerApp/Base/Extensions/UIColor.swift
@@ -10,5 +10,6 @@ import UIKit
 extension UIColor {
     
     static let black80 = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.8)
+    static let white50 = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)
     
 }
