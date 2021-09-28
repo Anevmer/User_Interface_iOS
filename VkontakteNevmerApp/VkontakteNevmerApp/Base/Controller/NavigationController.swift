1b2ab00b5c373d1f1f63b400f3316c2adc70e4bb
Anatoly <annevmerzhitsky@yandex.ru>
2021-09-28 17:12:56 +0300

    Pod_initial: Install pods

--- VkontakteNevmerApp/VkontakteNevmerApp/Base/Controller/NavigationController.swift
+++ VkontakteNevmerApp/VkontakteNevmerApp/Base/Controller/NavigationController.swift
@@ -46,56 +46,54 @@ class NavigationController: UINavigationController {
             navigationBar.scrollEdgeAppearance = appearance
             navigationBar.backgroundColor = appearance.backgroundColor
             
-            // --
-            
-            super.viewDidLoad()
-            delegate = self
-            let edgePanGR = UIScreenEdgePanGestureRecognizer(
-                target: self,
-                action: #selector(handlePan(_:)))
-            edgePanGR.edges = .left
-            view.addGestureRecognizer(edgePanGR)
+            // --            
+//            delegate = self
+//            let edgePanGR = UIScreenEdgePanGestureRecognizer(
+//                target: self,
+//                action: #selector(handlePan(_:)))
+//            edgePanGR.edges = .left
+//            view.addGestureRecognizer(edgePanGR)
             // --
         }
     }
     
     // --
     
-    @objc
-    private func handlePan(_ recognizer: UIScreenEdgePanGestureRecognizer) {
-        switch recognizer.state {
-        case .began:
-            interactiveTransition.isStarted = true
-            popViewController(animated: true)
-            
-        case .changed:
-            guard let width = recognizer.view?.bounds.width else {
-                interactiveTransition.isStarted = false
-                interactiveTransition.cancel()
-                return
-            }
-            
-            let translation = recognizer.translation(in: view)
-            let relativeTranslation = translation.x / width
-            let progress = max(0, min(relativeTranslation, 1))
-            print(translation, relativeTranslation , progress)
-            interactiveTransition.update(progress)
-            interactiveTransition.shouldFinish = progress > 0.35
-            
-        case .ended:
-            interactiveTransition.isStarted = false
-            interactiveTransition.shouldFinish ? interactiveTransition.finish() : interactiveTransition.cancel()
-            
-        case
-            .failed,
-            .cancelled:
-            interactiveTransition.isStarted = false
-            interactiveTransition.cancel()
-            
-        default: break
-        }
-    }
-    
+//    @objc
+//    private func handlePan(_ recognizer: UIScreenEdgePanGestureRecognizer) {
+//        switch recognizer.state {
+//        case .began:
+//            interactiveTransition.isStarted = true
+//            popViewController(animated: true)
+//
+//        case .changed:
+//            guard let width = recognizer.view?.bounds.width else {
+//                interactiveTransition.isStarted = false
+//                interactiveTransition.cancel()
+//                return
+//            }
+//
+//            let translation = recognizer.translation(in: view)
+//            let relativeTranslation = translation.x / width
+//            let progress = max(0, min(relativeTranslation, 1))
+//            print(translation, relativeTranslation , progress)
+//            interactiveTransition.update(progress)
+//            interactiveTransition.shouldFinish = progress > 0.35
+//
+//        case .ended:
+//            interactiveTransition.isStarted = false
+//            interactiveTransition.shouldFinish ? interactiveTransition.finish() : interactiveTransition.cancel()
+//
+//        case
+//            .failed,
+//            .cancelled:
+//            interactiveTransition.isStarted = false
+//            interactiveTransition.cancel()
+//
+//        default: break
+//        }
+//    }
+//
     // --
     
     override var preferredStatusBarStyle : UIStatusBarStyle {
@@ -138,27 +136,27 @@ class NavigationController: UINavigationController {
 }
 
 // --
-
-extension NavigationController: UINavigationControllerDelegate {
-    func navigationController(_ navigationController: UINavigationController,
-        animationControllerFor operation: UINavigationController.Operation,
-        from fromVC: UIViewController,
-        to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        switch operation {
-        case .push:
-            return pushAnimatior
-        case .pop:
-            return popAnimator
-        default:
-            return nil
-        }
-    }
-    
-    func navigationController(
-        _ navigationController: UINavigationController,
-        interactionControllerFor animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
-        interactiveTransition.isStarted ? interactiveTransition : nil
-    }
-}
-
+//
+//extension NavigationController: UINavigationControllerDelegate {
+//    func navigationController(_ navigationController: UINavigationController,
+//        animationControllerFor operation: UINavigationController.Operation,
+//        from fromVC: UIViewController,
+//        to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+//        switch operation {
+//        case .push:
+//            return pushAnimatior
+//        case .pop:
+//            return popAnimator
+//        default:
+//            return nil
+//        }
+//    }
+//
+//    func navigationController(
+//        _ navigationController: UINavigationController,
+//        interactionControllerFor animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+//        interactiveTransition.isStarted ? interactiveTransition : nil
+//    }
+//}
+//
 // --
